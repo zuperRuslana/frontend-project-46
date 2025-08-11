10e7970b966f091816723cba4884edf4b1c5e2bc
Ruslana <lanasigalova@gmail.com>
2025-08-11 17:52:19 -0400

    added files

--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,21 @@
 import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml'; 
+import { fileURLToPath } from 'url';
+
+
 
 export default function parse(filepath) {
-  const fullPath = path.resolve(process.cwd(), filepath);       
-  const data = fs.readFileSync(fullPath, 'utf-8');             
-  return JSON.parse(data);                                      
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fullPath = path.resolve(__dirname,'..','..', filepath);
+  const content = fs.readFileSync(fullPath,'utf-8')     
+  let ext = path.extname(filepath);
+  if (ext === '.json' ){
+     return JSON.parse(content);             
+  } else if(ext === '.yaml' || ext ==='.yml'){
+ return yaml.load(content); 
+}            
+throw new Error(`Unsupported file format: ${ext}`);                                    
 }
   
