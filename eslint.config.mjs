3bbe12d4dcfd85990231b5597405493efa7668f6
Ruslana <lanasigalova@gmail.com>
2025-11-25 22:46:35 -0500

    changed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    files: ["**/*.{js,mjs,cjs}"],
+    extends: [js.configs.recommended],
     languageOptions: {
       globals: {
-        ...globals.node,   // ← ВОТ ЭТА СТРОЧКА НУЖНА
+        ...globals.node,
       },
     },
   },
+
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: globals.browser },
+    files: ["__tests__/**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,   
+      },
+    },
   },
-  pluginReact.configs.flat.recommended,
 ]);
