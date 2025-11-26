7dc8d3b1fdd4ec825b46c8602ea1923545d7b580
Ruslana <lanasigalova@gmail.com>
2025-11-25 22:37:33 -0500

    changed esling config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -7,21 +8,15 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
     extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  {
-    files: ["**/__tests__/**/*", "**/*.test.js"],
     languageOptions: {
       globals: {
-        ...globals.node,
-        ...globals.jest,
+        ...globals.node,   // ← ВОТ ЭТА СТРОЧКА НУЖНА
       },
     },
   },
-
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
 ]);
